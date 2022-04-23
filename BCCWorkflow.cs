6767a6178a3f6ab242ef7391993161901ec6f305
McintirZeme <67140240+CodyFornof@users.noreply.github.com>
2022-04-22 18:15:31 -0700

    Create BCCWorkflow.cs

---
+++ BCCWorkflow.cs
@@ -0,0 +1,43 @@
+Company Name: <# <Content Select="//Company_Name" Optional="true"/> #>
+
+Company Full Name: <# <Content Select="//Company_Full_Name" Optional="true"/> #>
+
+Company City: <# <Content Select="//Company_City" Optional="true"/> #>
+
+Company Street: <# <Content Select="//Company_Street" Optional="true"/> #>
+
+Company State: <# <Content Select="//Company_State" Optional="true"/> #>
+
+Company Zip: <# <Content Select="//Company_Zip" Optional="true"/> #>
+
+Company Phone: <# <Content Select="//Company_Phone" Optional="true"/> #>
+
+Company Email: <# <Content Select="//Company_Email" Optional="true"/> #>
+
+Company Website: <# <Content Select="//Company_Website" Optional="true"/> #>
+
+Invoice Number: <# <Content Select="//Invoice_Number" Optional="true"/> #>
+
+Invoice Date: <# <Content Select="//Invoice_Date" Optional="true"/> #>
+
+Customers Name: <# <Content Select="//Customers_Name" Optional="true"/> #>
+
+Customer Street: <# <Content Select="//Customer_Street" Optional="true"/> #>
+
+Customer City: <# <Content Select="//Customer_City" Optional="true"/> #>
+
+Customer State: <# <Content Select="//Customer_State" Optional="true"/> #>
+
+Customer Zip: <# <Content Select="//Customer_Zip" Optional="true"/> #>
+
+Customer Phone: <# <Content Select="//Customer_Phone" Optional="true"/> #>
+
+Customer Email: <# <Content Select="//Customer_Email" Optional="true"/> #>
+
+
+
+
+
+
+
+
